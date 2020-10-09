c68a649908c86ff114fb309d8eb7bb6e34e9f0ed
sparsoya <sparsoya@ebay.com>
2020-10-08 23:27:48 -0700

    tableView: Fetch table data from Array

--- Memories/TableView/CountriesTableViewController.swift
+++ Memories/TableView/CountriesTableViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
+struct Country {
+	var isoCode: String
+	var name: String
+}
+
 class CountriesTableViewController : UITableViewController{
+	
+	let countries = [
+		Country(isoCode: "at", name: "Austria"),
+		Country(isoCode: "be", name: "Belgium"),
+		Country(isoCode: "de", name: "Germany"),
+		Country(isoCode: "el", name: "Greece"),
+		Country(isoCode: "fr", name: "France"),
+	]
 	
 	override func viewDidLoad() {
 		
@@ -18,24 +31,25 @@ class CountriesTableViewController : UITableViewController{
 	// MARK:- Table view data source
 	
 	override func numberOfSections(in tableView: UITableView) -> Int {
-		return 3
+		return 1
 	}
 	
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 5
+		return countries.count
 	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
 		
-		cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+		let country = countries[indexPath.row]
+		cell.textLabel?.text = country.name
 		
 		return cell
 	}
 	
-	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-		return "Section \(section)"
+/*	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return "Section Header"
 	}
-	
+*/
 }
